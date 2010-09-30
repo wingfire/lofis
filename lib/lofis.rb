fd5f4b8514283b7d6e2b0ee1a9cc9e5780e397f9
Moritz Heidkamp <moritz.heidkamp@bevuta.com>
2010-09-30 19:19:39 +0200

    auto-load the stock lofis.yml and make Lofis a module which can be included in String

--- lib/lofis.rb
+++ lib/lofis.rb
@@ -6,6 +6,7 @@ module Lofis
   end
 
   def self.mapping
+    @mapping ||= load_file(File.join(File.dirname(__FILE__), '..', 'lofis.yml'))
     @mapping || raise(Error, 'no lofis file loaded')
   end
 
@@ -13,10 +14,14 @@ module Lofis
     @mapping = YAML.load_file(file)['umlaut'].map { |r,s| [Regexp.new(s.map { |c| Regexp.escape(c) }.join('|')), r] }
   end
 
-  def self.convert(s)
-    @mapping.inject(s) do |g,(s,r)|
+  def self.to_sortable(s)
+    mapping.inject(s) do |g,(s,r)|
       g.gsub(s, r)
     end
+  end
+
+  def to_sortable
+    @_lofis_sortable ||= Lofis.to_sortable(self)
   end
   
 end
